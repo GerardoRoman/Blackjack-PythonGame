56fd08d9c1aaec2d05b8d9966669a23997bedf38
GerardoRoman <offthegrd3657@gmail.com>
2023-02-22 00:01:01 -0500

    added/modified logic for Dealer and Game classes with some help from youtube and the interwebs, and planning on reviewing some of the code I don't understand and make notes for myself

--- blackjack.py
+++ blackjack.py
@@ -30,8 +30,7 @@ class Deck: # represents a deck of standard playing cards
         random.shuffle(self.cards)
     
 
-class Player:
-# represents a player in the game 
+class Player: # represents a player in the game 
     def __init__(self):
         self.name = input("What is your name? ")
         self.hand = []
@@ -44,7 +43,7 @@ class Player:
             print(f"\n{self.name}'s hand: ") # \n represents new line break for a cleaner look
             for card in self.hand:
                 print(card)
-            print(f"Total: {self.get_hand_value()}")
+            print(f"Total: {self.calculate_hand_value()}")
             decision = input("\nDo you want to hit or stay? ")
             if decision.lower() == 'hit':
                 print(F"{self.name} busts!")
@@ -52,6 +51,22 @@ class Player:
             else:
                 return 'stay'
             
+    def calculate_hand_value(self):
+        hand_value = 0
+        aces = 0
+        for card in self.hand:
+            if card.rank == 'A':
+                aces += 1
+            elif card.rank in ['K', 'Q', 'J']:
+                hand_value += 10
+            else:
+                hand_value += card.rank
+        for i in range(aces):
+            if hand_value + 11 > 21:
+                hand_value += 1
+            else:
+                hand_value += 11
+        return hand_value
 
 
 class Dealer(Player): # inherits from Player, init will be the same but turn will be different # everything is the same as player, unless you write it differently in here
@@ -62,16 +77,18 @@ class Dealer(Player): # inherits from Player, init will be the same but turn wil
     def __str__(self):
         return f'{self.name} is the dealer'
     
-    
-    
     def turn(self):
-        '''unlike player, dealer follows house rules and stays at 17, no choice'''
+        while self.calculate_hand_value() < 17:
+            card = deck.deal_card()
+            self.hand.append(card)
+            if self.calculate_hand_value() > 21:
+                print(f"{self.name} busts!")
+                return 'bust'
+            else:
+                return 'stay'
 
-    def end_game(self):
-        pass
 
-
-class Game:
+class Game: 
     def __init__(self, deck=None): # can set default values in the signature line, like 'deck=None'
         self.player = Player() # the value of self.player is an instance of the class Player
         self.dealer = Dealer()
@@ -82,28 +99,56 @@ class Game:
         self.deck.add_cards()
     
     def player_turn(self):
-        pass
+        result = self.player.turn(self.deck)
+        if result == 'bust':
+            self.end_game(self.dealer)
+        else:
+            self.dealer_turn()
+    
+    def dealer_turn(self):
+        result = self.dealer.turn(self.deck)
+        if result == 'bust':
+            self.end_game(self.player)
+        else:
+            self.end_game()
+
+# sets game-ending parameters
+    def end_game(self, winner=None):
+        
+    # print player's hand and total
+        print(f"\n{self.player.name}'s hand:")
+        for card in self.player.hand:
+            print(card)
+        print(f"Total: {self.player.calculate_hand_value()}")
+        
+    # print dealer's hand and total
+        print(f"\n{self.dealer.name}'s hand:")
+        for card in self.dealer.hand:
+            print(card)
+        print(f"Total: {self.dealer.calculate_hand_value()}")
+    
+    # determine winner
+        
 
 
 new_game = Game()
 
+# new_game.deck.shuffle()
+# card = new_game.deck.deal_card()
+# new_game.dealer.hand.append(card)
+# card = new_game.deck.deal_card()
+# new_game.dealer.hand.append(card)
+# print(new_game.dealer)
+# for card in new_game.dealer.hand:
+#     print(card)
 
-new_game.deck.shuffle()
-card = new_game.deck.deal_card()
-new_game.dealer.hand.append(card)
-card = new_game.deck.deal_card()
-new_game.dealer.hand.append(card)
-print(new_game.dealer)
-for card in new_game.dealer.hand:
-    print(card)
-
-card = new_game.deck.deal_card()
-new_game.player.hand.append(card)
-card = new_game.deck.deal_card()
-new_game.player.hand.append(card)
-print(new_game.player)
-for card in new_game.player.hand:
-    print(card)
+# card = new_game.deck.deal_card()
+# new_game.player.hand.append(card)
+# card = new_game.deck.deal_card()
+# new_game.player.hand.append(card)
+# print(new_game.player)
+# for card in new_game.player.hand:
+#     print(card)
 
 # TODO
 # ✅ make a player, like we did with Deck 
